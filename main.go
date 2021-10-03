6092ac77f6cc6f6d41ade01720efe87e56f927cf
soheyHashimoto <heyso1209@gmail.com>
2021-10-03 20:12:42 +0900

    Add time watcher

--- main.go
+++ main.go
@@ -6,13 +6,30 @@ import (
 )
 
 func main() {
-	dena, ok := scraper.ScrapeDeNA()
-	if ok {
-		log.Println(dena)
-	}
+	start := time.Now()
+	concurrent()
+	end := time.Now()
+	log.Printf("%f 秒時間がかかりました\n", (end.Sub(start)).Seconds())
+}
 
-	zozo, ok := scraper.ScrapeZOZO()
-	if ok {
-		log.Println(zozo)
-	}
+func concurrent() {
+	wg := &sync.WaitGroup{}
+	wg.Add(siteNum)
+	go func() {
+		defer wg.Done()
+		dena, ok := scraper.ScrapeDeNA()
+		if ok {
+			log.Println(dena)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		zozo, ok := scraper.ScrapeZOZO()
+		if ok {
+			log.Println(zozo)
+		}
+	}()
+
+	wg.Wait()
 }
