7c52257243bf95fb58cc510dd3a8acced1aa28d9
soheyHashimoto <heyso1209@gmail.com>
2021-10-24 01:30:30 +0900

    [REMOVE]ScrapeCookpad

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -136,36 +136,3 @@ func ScrapeZOZO() (Article, bool) {
 
 	return article, false
 }
-
-// ScrapeCookpad クックパッドのテックブログの最新記事が今日のだったら構造体Articleと存在の有無をboolで返すメソッド
-func ScrapeCookpad() (Article, bool) {
-	url := "https://techlife.cookpad.com/"
-	res, err := http.Get(url)
-	if err != nil {
-		log.Println(err)
-	}
-
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-
-	latestArticleDate := doc.Find("time").First().Text()
-	var article Article
-	// NOTE: timeパッケージではformatを指定する際には2006-01-02にする
-	// (アメリカ式の時刻の順番。"1月2日午後3時4分5秒2006年"（つまり「自然な順番」で1, 2, 3, 4, 5, 6）を指している)
-	if strings.Contains(latestArticleDate, time.Now().Format("2006-01-02")) {
-		title := doc.Find(".entry-title > a").First()
-		articleLink, exist := title.Attr("href")
-		if !exist {
-			log.Println("error")
-		}
-
-		article = Article{
-			Company: "クックパッド",
-			Title:   title.Text(),
-			Url:     articleLink,
-		}
-
-		return article, true
-	}
-
-	return article, false
-}
