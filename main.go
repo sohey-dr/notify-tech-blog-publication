25668ece0be42cb404cb0fb9fc5fe7f29b31b9af
soheyHashimoto <heyso1209@gmail.com>
2021-10-24 15:20:24 +0900

    [ADD]Cyber Agent

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func run() {
 	articles := concurrentScraping(
 		scraper.NewScraper("DeNA", "https://engineer.dena.com/", ".article-list", time.Now().Format("January 02, 2006"), "div > h2 > a", false).Scrape,
 		scraper.NewScraper("ZOZO", "https://techblog.zozo.com/", "time", time.Now().Format("2006-01-02"), ".entry-title > a", true).Scrape,
+		// TODO: Slackで読み込み後に見れるため見逃しているがタイトルも取得するようにする
+		scraper.NewScraper("Cyber Agent", "https://developers.cyberagent.co.jp/blog/", "time", time.Now().Format("2006/01/02"), ".card__time_title > a", true).Scrape,
 		scraper.NewScraper("クックパッド", "https://techlife.cookpad.com/", "time", time.Now().Format("2006-01-02"), ".entry-title > a", true).Scrape,
 	)
 	if len(articles) != 0 {
